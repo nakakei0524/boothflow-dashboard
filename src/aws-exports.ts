d54571c68347c2112d095a1751323087f06e1c2a
nakakei0524 <k.nakazato@memori-first.com>
2025-07-18 19:47:02 +0900

    userPoolIdにデフォルト値をセットし、必ずログインできるよう修正

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -2,7 +2,7 @@ import { Amplify } from 'aws-amplify';
 
 const authConfig = {
   Cognito: {
-    userPoolId: process.env.REACT_APP_USER_POOL_ID || "", // .envで管理、なければ空文字列
+    userPoolId: process.env.REACT_APP_USER_POOL_ID || "ap-northeast-1_gOiAtMjfB", // デフォルト値をセット
     userPoolClientId: "4g55pakh2lpm1pki6jhumscnhl",
     signUpVerificationMethod: "code" as const,
   }
